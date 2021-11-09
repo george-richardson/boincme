e60fbe4f4f50e755c5a1485e66b30019a076235f
George Richardson <george@gjhr.me>
2021-11-09 22:30:18 +0000

    Allow dry run on delete amis script. Ignore packer build if packer code did not change.

--- misc/delete-all-amis-all-regions.sh
+++ misc/delete-all-amis-all-regions.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+set -e
+DRY_RUN=true
+
+while getopts "y" opt
+do
+  case "$opt" in
+    "y") DRY_RUN=false ;;
+  esac
+done
+
+! $DRY_RUN || echo "!! This is a dry run. Use -y for wet run. !!"
+
 for REGION in $(aws ec2 describe-regions --query "Regions[].RegionName" --output text); do 
   echo "REGION: $REGION"
-  for AMI in $(aws ec2 describe-images --region $REGION --owners self --query "Images[].ImageId" --output text); do
+  for AMI in $(aws ec2 describe-images --region $REGION --owners self --filter Name=state,Values=available --query "Images[].ImageId" --output text); do
     echo "- AMI: $AMI"
-    aws ec2 deregister-image --region $REGION --image-id $AMI
+    $DRY_RUN || (aws ec2 deregister-image --region $REGION --image-id $AMI && echo "  Deleted")
   done
 done
